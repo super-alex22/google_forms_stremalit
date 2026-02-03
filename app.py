01ea054c260b30bbbd7a58aaec4fed72af3b1536
super-alex22 <116815031+super-alex22@users.noreply.github.com>
2026-02-03 20:45:54 +0200

    Add name input prompt to geography quiz

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.write("This is a geography quiz. Let's see how good your skills are")
+st.write("I'd like humbly have you requested to enter your name here")
+name = st.text_input("Have your name stated here")
